a6414a1ed675b1a0a10911fc26dcfc6ebc68e399
medjnfgxgh <lewie.dudding@gmail.com>
2026-03-25 16:42:39 +1300

    pet getty

--- gatito.js
+++ gatito.js
@@ -1,7 +1,15 @@
+var pet = 0;
+
 function gatito() {
     console.log("gatito");
     let val = prompt("gatito?");
 
     if (val.toLowerCase().startsWith("y"))
         window.location = "https://drewfitzgerald.co.nz";
+}
+
+function pet_getty() {
+    pet+=1;
+
+    document.getElementById('c').innerHTML =  pet;
 }
